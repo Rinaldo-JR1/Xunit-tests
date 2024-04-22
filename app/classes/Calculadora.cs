d3224d8f66a65984f3243b06abe9cf234834a809
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-04-22 14:28:45 -0300

    Fim com os testes

--- app/classes/Calculadora.cs
+++ app/classes/Calculadora.cs
@@ -1,30 +1,55 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
 namespace app.classes
 {
+
+
     public class Calculadora
     {
+
+        private List<string> ListaHistorico;
+        public Calculadora()
+        {
+            ListaHistorico = new List<string>();
+        }
+
         public int Somar(int a, int b)
         {
-            return a + b;
+            int res = a + b;
+            ListaHistorico.Insert(0, $"Res: {res} \n");
+            return res;
         }
         public int Subtrair(int a, int b)
         {
+            int res = a - b;
+            ListaHistorico.Insert(0, $"Res: {res} \n");
             return a - b;
         }
         public int Multiplicar(int a, int b)
         {
-            return a * b;
+            int res = a * b;
+            ListaHistorico.Insert(0, $"Res: {res} \n");
+            return res;
         }
         public int Dividir(int a, int b)
         {
-            return a / b;
+            if (b == 0)
+            {
+                throw new DivideByZeroException();
+            }
+            int res = a / b;
+            ListaHistorico.Insert(0, $"Res: {res} \n");
+            return res;
         }
-        public List<string> Historico(){
-            return new List<string>();
+        public List<string> Historico()
+        {
+
+            ListaHistorico.RemoveRange(4, ListaHistorico.Count - 4);
+            return ListaHistorico;
         }
     }
 }
--- tests/UnitTest1.cs
+++ tests/UnitTest1.cs
@@ -5,11 +5,53 @@ namespace tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    Calculadora calc = new Calculadora();
+
+    [Theory]
+    [InlineData(1, 2, 3)]
+    public void TesteSoma(int val1, int val2, int resultadoEsperado)
     {
-        Calculadora calc = new Calculadora();
-        int resultado = calc.Somar(1, 1);
-        Assert.Equal(2, resultado);
+
+        int resultadoSoma = calc.Somar(val1, val2);
+        Assert.Equal(resultadoEsperado, resultadoSoma);
+    }
+    [Theory]
+    [InlineData(4, 2, 2)]
+    public void TesteSubtracao(int val1, int val2, int resultadoEsperado)
+    {
+
+        int resultadoSubtracao = calc.Subtrair(val1, val2);
+        Assert.Equal(resultadoEsperado, resultadoSubtracao);
+    }
+    [Theory]
+    [InlineData(2, 2, 4)]
+    public void TesteMultiplicacao(int val1, int val2, int resultadoEsperado)
+    {
+
+        int resultadoMultiplicacao = calc.Multiplicar(val1, val2);
+        Assert.Equal(resultadoEsperado, resultadoMultiplicacao);
+    }
+    [Theory]
+    [InlineData(4, 2, 2)]
+    public void TesteDivisao(int val1, int val2, int resultadoEsperado)
+    {
+        int resultadoDivisao = calc.Dividir(val1, val2);
+        Assert.Equal(resultadoEsperado, resultadoDivisao);
+    }
+    [Fact]
+    public void TesteDivisaPor0()
+    {
+        Assert.Throws<DivideByZeroException>(() => calc.Dividir(4, 0));
+    }
+    [Fact]
+    public void TestarHistorico()
+    {
+        calc.Somar(1, 2);
+        calc.Somar(2, 2);
+        calc.Somar(3, 3);
+        calc.Somar(4, 4);
+        List<string> historico = calc.Historico();
+        Assert.NotEmpty(historico);
+        Assert.Equal(4, historico.Count);
     }
 }
